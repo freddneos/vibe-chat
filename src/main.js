fa3a660007ecff5d51e6919438900502642ddc44
freddneos <fredd@neosdev.com.br>
2025-05-27 21:17:33 +0100

    feat: add snes.css dependency and update main.js for styling

--- src/main.js
+++ src/main.js
@@ -1,21 +1,22 @@
 import './style.css'
+import 'snes.css/dist/snes.css' // Import snes.css
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
-  <div>
+  <div class="snes-container" style="background-color: #000;">
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <h1 class="snes-text">Hello Vite!</h1>
+    <div class="card snes-panel">
+      <button id="counter" type="button" class="snes-button"></button>
     </div>
-    <p class="read-the-docs">
+    <p class="read-the-docs snes-text">
       Click on the Vite logo to learn more
     </p>
   </div>
